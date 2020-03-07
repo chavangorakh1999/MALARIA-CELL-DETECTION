4448bd38af5a72f515496855cf04036b37b8ff95
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-03-07 09:40:08 +0530

    malaria cell detection with 99.54% accuracy

--- cnn.py
+++ cnn.py
@@ -52,32 +52,30 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('cell_images/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('cell_images/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
+                         steps_per_epoch = 11080,
                          epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 2000)
-
-# Part 3 - Making new predictions
-
+                         validation_steps = 2700)
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img('dataset/single_prediction/Infected_or_NotInfected.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
 training_set.class_indices
 if result[0][0] == 1:
-    prediction = 'dog'
+    prediction = 'Infected'
 else:
-    prediction = 'cat'
+    prediction = 'NotInfected'
+
